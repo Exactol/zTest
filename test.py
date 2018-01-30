b9d5eecc489ff28effd5862e9163e20bae9b0e53
Zach <zachmatuson@gmail.com>
2018-01-29 20:58:21 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+from zTest import zTest
+
+def y_n(prompt="Answer yes or no\n"):
+     while True:
+         answer = input(prompt)
+         if answer in ['y', 'Y', 'yes']:
+             print ("You said yes!")
+             break
+         elif answer in ['n', 'N', 'no']:
+             print ("You said no!")
+             break
+         else:
+             print ("%s is an invalid answer."%answer)
+
+testClass = zTest(["y", "n"])
+y_n()
+y_n()
---
+++ zTest.py
@@ -0,0 +1,46 @@
+import inspect
+
+#index of test input
+index = -1
+testStrings = []
+originalInput = input
+raiseExceptionOnTestFinished = False
+callingModule = None
+
+#Input override to simulate input at index of test string
+def testInput(printStr):
+    print(printStr)
+
+    global index
+    index += 1
+
+    #If out of index, return control to user
+    if (index > len(testStrings) - 1):
+        callingModule.input = originalInput
+        print("{:->30}".format("-"), "Out of test cases, returning to user input", "{:->30}".format("-\n"))
+
+        #Either raise exception or return empty string
+        if (raiseExceptionOnTestFinished):
+            raise Exception("Out of test cases, returning to user input")
+        else:
+            retStr = input()
+            return(retStr)
+    else:
+        print("-Test:", testStrings[index])
+        #Returning string is equivilent of user input
+        return testStrings[index]
+
+class zTest:
+    def __init__(self, testStrs, raiseException = False):
+        #Get calling module by inspecting the parent frame of calling function
+        global callingModule
+        callingModule = inspect.getmodule(inspect.stack()[1][0])
+
+        #Override calling function's input function
+        callingModule.input = testInput
+        
+        global testStrings
+        testStrings = testStrs
+
+        global raiseExceptionOnTestFinished
+        raiseExceptionOnTestFinished = raiseException
